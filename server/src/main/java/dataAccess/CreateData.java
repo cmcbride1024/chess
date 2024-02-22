d2c5abad5ad08a7d7a99e689745249063dd0a2f8
connor.mcbride <127568285+cmcbride1024@users.noreply.github.com>
2024-02-22 11:06:42 -0700

    Created Data Access files

---
+++ server/src/main/java/dataAccess/CreateData.java
@@ -0,0 +1,22 @@
+package dataAccess;
+
+import model.GameData;
+import model.UserData;
+
+public class CreateData {
+    void createUser(UserData u) throws DataAccessException {
+
+    }
+
+    void createAuth(UserData u) throws DataAccessException {
+
+    }
+
+    void createGameID(GameData g) throws  DataAccessException {
+
+    }
+
+    void createGame(GameData g) throws  DataAccessException {
+
+    }
+}
