618962fa4bd6cc4f031c15f50c263ec72829a0c1
connor.mcbride <127568285+cmcbride1024@users.noreply.github.com>
2024-02-28 18:56:07 -0700

    Implemented clearApplication endpoint

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,7 +1,13 @@
 import chess.*;
+import dataAccess.MemoryDataAccess;
+import server.*;
 
 public class Main {
     public static void main(String[] args) {
+        Server server = new Server();
+        int port = 8080;
+        server.run(port);
+
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
     }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,18 +1,35 @@
 package server;
 
+import com.google.gson.Gson;
+import dataAccess.MemoryDataAccess;
+import service.UserService;
 import spark.*;
 
 public class Server {
-
+    private final Gson gson = new Gson();
+    private final UserService service = new UserService(new MemoryDataAccess());
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", this::clearApplication);
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private Object clearApplication(Request req, Response res) {
+        try {
+            service.clearApplication();
+            res.status(200);
+            return "";
+        } catch (Exception e) {
+            res.status(500);
+            res.type("application/json");
+            return gson.toJson(new ResponseMessage("message", "Error: description"));
+        }
     }
 
     public void stop() {
@@ -20,3 +37,22 @@ public class Server {
         Spark.awaitStop();
     }
 }
+ class ResponseMessage {
+    private final String header;
+    private final String body;
+
+    public ResponseMessage(String header, String body) {
+        this.header = header;
+        this.body = body;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
+
+
