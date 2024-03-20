50958a70011cd807cdfd52ce18002a2d564de025
connor.mcbride <127568285+cmcbride1024@users.noreply.github.com>
2024-03-19 21:49:22 -0600

    Added register() test

---
+++ client/src/main/java/server/ServerFacade.java
@@ -0,0 +1,14 @@
+package server;
+
+import model.AuthData;
+
+public class ServerFacade {
+    private final int port;
+    public ServerFacade(int port) {
+        this.port = port;
+    }
+
+    public AuthData register(String username, String password, String email) {
+        return null;
+    }
+}
--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -2,17 +2,22 @@ package clientTests;
 
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
     }
 
     @AfterAll
@@ -20,10 +25,9 @@ public class ServerFacadeTests {
         server.stop();
     }
 
-
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    void register() {
+        var authData = facade.register("stevejobs", "lotsofmoney", "sjobs@icloud.com");
+        assertTrue(authData.authToken().length() > 10);
     }
-
 }
