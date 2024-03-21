2db240d13923f199fe4501e9c5368b1d0c3a9ff0
connor.mcbride <127568285+cmcbride1024@users.noreply.github.com>
2024-03-20 19:41:58 -0600

    Added Facade methods

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,6 +1,6 @@
 package client;
 
-import ui.EscapeSequences;
+import exception.ResponseException;
 
 import java.util.Arrays;
 
@@ -14,14 +14,37 @@ public class ChessClient {
     }
 
     public String eval(String input) {
-        var tokens = input.toLowerCase().split(" ");
-        var cmd = (tokens.length > 0) ? tokens[0] : "help";
-        var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
 
-        return switch(cmd) {
+            return switch(cmd) {
+                case "login" -> login(params);
+                case "register" -> register(params);
+                case "create" -> create(params);
+                case "list" -> listGames();
+                case "join" -> joinGame(params);
+                case "observe" -> observe(params);
+                case "logout" -> logout(params);
+                case "quit" -> "quit";
+                default -> help();
+            };
+        } catch (ResponseException ex) {
+            return ex.getMessage();
+        }
+    }
 
-            default -> help();
-        };
+    public String login(String... params) throws ResponseException {
+        if (params.length >= 1) {
+            state = State.SIGNEDIN;
+
+        }
+        throw new ResponseException(400, "Expected: <username> <password>");
+    }
+
+    public String register(String... params) throws ResponseException {
+
     }
 
     public String help() {
@@ -44,5 +67,11 @@ public class ChessClient {
             help - with possible commands
             
             """;
+    }
+
+    private void assertSignedIn() throws ResponseException {
+        if (state == State.SIGNEDOUT) {
+            throw new ResponseException(400, "You must sign in.");
+        }
     }
 }
