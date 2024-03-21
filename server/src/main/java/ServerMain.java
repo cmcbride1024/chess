113847471ef53cc768d8e7170ea83f745c6b479c
connor.mcbride <127568285+cmcbride1024@users.noreply.github.com>
2024-03-21 02:31:43 -0600

    Fixed ServerMain.java issues

--- server/src/main/java/ServerMain.java
+++ server/src/main/java/ServerMain.java
@@ -1,13 +1,16 @@
-import chess.*;
 import client.*;
 
 public class ServerMain {
     public static void main(String[] args) {
-        Server server = new Server();
-        int port = 8080;
-        server.run(port);
-
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        try {
+            int port = 8080;
+            if (args.length >= 1) {
+                port = Integer.parseInt(args[0]);
+            }
+            var portNumber = new Server().run(port);
+            System.out.printf("Server started on port %d%n", portNumber);
+        } catch (Throwable ex) {
+            System.out.printf("Unable to start server: %s%n", ex.getMessage());
+        }
     }
 }
